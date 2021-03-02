2cb83ed69caf201fbeca6465cbf2f79985c28484
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2021-03-02 22:34:05 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import bitcoinRequest from "./api/BitcoinRequest";
 
 export default function App() {
+  const [time, setTime] = useState({});
+  const [bpi, setBpi] = useState({});
+  const [disclaimer,setDisclaimer] = useState("");
+  const [chosenBpi, setChosenBpi] = useState("USD");
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const response = await bitcoinRequest.get();
+      setTime(response.data.time);
+      setDisclaimer(response.data.disclaimer);
+      if (chosenBpi === "USD")
+      {
+         setBpi(response.data.bpi.USD);
+      }
+      if (chosenBpi === "EUR")
+      {
+         setBpi(response.data.bpi.EUR);
+      }
+      if (chosenBpi === "GBP")
+      {
+         setBpi(response.data.bpi.GBP);
+      }
+    };
+    doTranslation();
+  },[chosenBpi]);
+
+  const onButtonPressed = (term) => {
+    if(term === "USD")
+    {
+      setChosenBpi("USD")
+    }
+    if(term === "EUR")
+    {
+      setChosenBpi("EUR")
+    }
+    if(term === "GBP")
+    {
+      setChosenBpi("GBP")
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>1 bitcoin = {bpi.rate} {bpi.description}</Text>
+      <Text>{time.updated}</Text>
+      <Button title="USD" onPress={() => onButtonPressed("USD")} />
+      <Button title="EUR" onPress={() => onButtonPressed("EUR")} />
+      <Button title="GBP" onPress={() => onButtonPressed("GBP")} />
+      <Text>{disclaimer}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +60,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center"
   },
 });
